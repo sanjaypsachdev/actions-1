3071fe8335ebad465ab50eb114e5908de631859a
Sanjay Sachdev <ssachdev.pros@gmail.com>
2024-04-11 17:46:01 +0000

    fixed another error

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-run: sudo apt-get install cowsay -y
-run: cowsay -f dragon "Run for cover, I am a DRAGON.....RAWR" >> dragon.txt
-run: grep -i "dragon" dragon.txt
-run: cat dragon.txt
+sudo apt-get install cowsay -y
+cowsay -f dragon "Run for cover, I am a DRAGON.....RAWR" >> dragon.txt
+grep -i "dragon" dragon.txt
+cat dragon.txt
